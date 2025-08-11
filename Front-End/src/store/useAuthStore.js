3ef7a52ed6ed72da0b53ebd713239b1a10730843
inankarim <inankarimchowdhury@gmail.com>
2025-08-12 02:17:53 +0600

    route-setup

---
+++ Front-End/src/store/useAuthStore.js
@@ -0,0 +1,22 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isSigningUp: false,
+  isLoggingng: false,
+  isUpdatingprofile: true,
+  isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log("Error in checkAuth:", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+}));
