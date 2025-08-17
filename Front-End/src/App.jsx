7917e4dc53636bbd56752637275ededb6d78d657
inankarim <inankarimchowdhury@gmail.com>
2025-08-17 19:44:35 +0600

    inan to faiyaz

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,19 +1,17 @@
 import Navbar from "./components/Navbar";
-
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
-
+import Dashboard from "./pages/Dashboard";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
 import { useThemeStore } from "./store/useThemeStore";
 import { useEffect } from "react";
-
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
-
+import QuizHomePage from "./pages/QuizHomePage";
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
   const { theme } = useThemeStore();
@@ -38,7 +36,10 @@ const App = () => {
       <Navbar />
 
       <Routes>
-        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/chathome" element={authUser ? <HomePage /> : <Navigate to="/" />} />
+        <Route path="/" element={authUser ? <Dashboard /> : <Navigate to="/login" />} />
+        <Route path="/quiz" element={authUser ? <QuizHomePage/> : <Navigate to="/login" />} />
+
         <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
         <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
         <Route path="/settings" element={<SettingsPage />} />
---
+++ Front-End/src/pages/QuizHomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QuizHomePage = () => {
+    return (
+        <div>
+            this is quiz
+        </div>
+    );
+};
+
+export default QuizHomePage;
