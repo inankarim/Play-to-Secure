3a2d9d012be8811a0b3fd0d747aaa28c3f2d4008
inankarim <inankarimchowdhury@gmail.com>
2025-08-30 09:30:41 +0600

    Update socket.js

--- BackEnd/src/lib/socket.js
+++ BackEnd/src/lib/socket.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: { 
-    origin: ["http://localhost:5173", "http://localhost:5001"], 
+    origin: ["https://play-to-secure-ec7f.vercel.app/", "http://localhost:5001"], 
     credentials: true,
     methods: ["GET", "POST"]
   },
